adf821cc88456ca0f45cdd5e794755b048ba1db6
stardiviner <numbchild@gmail.com>
2020-07-04 23:21:51 +0800

    remove subtract timestamp function version 1

--- ffmpeg.el
+++ ffmpeg.el
@@ -1,6 +1,6 @@
 ;;; ffmpeg.el --- FFmpeg command utilities wrappers -*- lexical-binding: t; -*-
 
-;;; Time-stamp: <2020-07-04 23:20:35 stardiviner>
+;;; Time-stamp: <2020-07-04 23:21:44 stardiviner>
 
 ;; Authors: stardiviner <numbchild@gmail.com>
 ;; Package-Requires: ((emacs "25.1"))
@@ -29,47 +29,8 @@
 
 ;;; Code:
 
-(defun ffmpeg--convert-number-list-to-timestamp (number-list)
-  "Convert timestamp NUMBER-LIST to timestamp string."
-  (let ((hour (car number-list))
-        (minute (cadr number-list))
-        (second (caddr number-list)))
-    (mapconcat 'concat (mapcar
-                        (lambda (num)
-                          (format "%02d" num))
-                        (list hour minute second)) ":")))
-
-;; (ffmpeg--convert-number-list-to-timestamp '(0 0 12))
-
-(defun ffmpeg--subtract-timestamps-1 (start-timestamp end-timestamp)
-  "Subtract END-TIMESTAMP with START-TIMESTAMP."
-  (let* ((start-t (mapcar 'string-to-number (split-string start-timestamp ":")))
-         (start-hour (car start-t))
-         (start-minute (cadr start-t))
-         (start-second (caddr start-t))
-         (end-t (mapcar 'string-to-number (split-string end-timestamp ":")))
-         (end-hour (car end-t))
-         (end-minute (cadr end-t))
-         (end-second (caddr end-t))
-         (subtract-t (list (- end-hour start-hour)
-                           (- end-minute start-minute)
-                           (- end-second start-second))))
-    ;; TODO convert number to two number string. 00, 01 etc
-    (when (< (caddr subtract-t) 0)
-      (setf subtract-t (list (car subtract-t)
-                             (1- (cadr subtract-t))
-                             (- (caddr subtract-t)))))
-    (when (< (cadr subtract-t) 0)
-      (setf subtract-t (list (1- (car subtract-t))
-                             (- (cadr subtract-t))
-                             (caddr subtract-t))))
-    (ffmpeg--convert-number-list-to-timestamp subtract-t)))
-
-;; (ffmpeg--subtract-timestamps-1 "00:11:25" "00:12:12")
-
 ;;; NOTE Because ffmpeg command option "-t" accept seconds like 57 as value.
-
-(defun ffmpeg--subtract-timestamps-3 (start-timestamp end-timestamp)
+(defun ffmpeg--subtract-timestamps (start-timestamp end-timestamp)
   "Subtract END-TIMESTAMP with START-TIMESTAMP."
   (time-subtract
    (encode-time (parse-time-string
@@ -77,7 +38,7 @@
    (encode-time (parse-time-string
                  (concat "2020-01-01T" start-timestamp)))))
 
-;; (ffmpeg--subtract-timestamps-3 "00:11:25" "00:12:12")
+;; (ffmpeg--subtract-timestamps "00:11:25" "00:12:12")
 
 (defun ffmpeg-cut-clip (input-filename start-timestamp end-timestamp output-filename)
   "Cut clip of media INPUT-FILENAME between START-TIMESTAMP END-TIMESTAMP and output to OUTPUT-FILENAME."
@@ -92,7 +53,7 @@
    :command (list "ffmpeg"
                   "-i" input-filename
                   "-ss" start-timestamp
-                  "-t" (ffmpeg--subtract-timestamps-3 start-timestamp end-timestamp)
+                  "-t" (ffmpeg--subtract-timestamps start-timestamp end-timestamp)
                   "-codec" "copy"
                   output-filename)
    :buffer "*ffmpeg-cut-clip*"
