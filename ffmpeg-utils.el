2c4db969c161da83956a685b4c008490dfe7de3e
stardiviner <numbchild@gmail.com>
2023-02-06 09:07:28 +0800

    Fix checkdoc warnings, package-lint errors, and melpazoid suggestions.

--- ffmpeg-utils.el
+++ ffmpeg-utils.el
@@ -4,7 +4,7 @@
 
 ;; Authors: stardiviner <numbchild@gmail.com>
 ;; Package-Requires: ((emacs "25.1") (alert "1.2") (transient "0.1.0") (osx-lib "0.1"))
-;; Package-Version: 0.1
+;; Version: 0.1
 ;; Keywords: multimedia
 ;; homepage: https://repo.or.cz/ffmpeg-utils.git
 
@@ -25,7 +25,7 @@
 ;;; Commentary:
 
 ;; Usage:
-;;; - [M-x ffmpeg-cut-clip]
+;;; - [M-x ffmpeg-utils-cut-clip]
 
 ;;; Code:
 
@@ -33,19 +33,19 @@
 (require 'alert)
 (require 'transient)
 
-(defvar ffmpeg--output-filename nil
+(defvar ffmpeg-utils--output-filename nil
   "A variable to store the command output filename which used in notification.")
 
-(defcustom ffmpeg-notification-function 'ffmpeg-notification-default
+(defcustom ffmpeg-utils-notification-function 'ffmpeg-utils-notification-default
   "Specify the ffmpeg command process sentinel function."
   :type 'function
   :safe #'functionp
   :group 'ffmpeg)
 
-(defun ffmpeg-notification-default (&optional proc event)
+(defun ffmpeg-utils-notification-default (&optional proc event)
   "The ffmpeg command process sentinel notification function with args PROC, EVENT."
   (setq mode-line-process nil)     ; remove mode-line-process indicator.
-  (let ((msg (format "ffmpeg cut %s finished" (file-name-nondirectory ffmpeg--output-filename))))
+  (let ((msg (format "ffmpeg cut %s finished" (file-name-nondirectory ffmpeg-utils--output-filename))))
     (pcase system-type
       ('gnu/linux
        (cond
@@ -74,18 +74,18 @@
          (shell-command
           (format "osascript -e 'display notification \"%s\" with title \"%s\"'"
                   "yes" "Emacs ffmpeg-utils.el")))))
-      (_ (message (format "Emacs ffmpeg-utils.el: %s" msg))))))
+      (_ (message "Emacs ffmpeg-utils.el: %s" msg)))))
 
-(defun ffmpeg--run-command (arglist)
+(defun ffmpeg-utils--run-command (arglist)
   "Construct ffmpeg command with ARGLIST, SENTINEL-FUNC and BODY."
   (make-process
    :name "ffmpeg"
    :command (append '("ffmpeg") arglist) ; <------------- problem here
    :buffer "*ffmpeg*"
-   :sentinel ffmpeg-notification-function))
+   :sentinel ffmpeg-utils-notification-function))
 
-(defun ffmpeg-mode-line-running-indicator (msg)
-  "Display a running indicator on mode-line."
+(defun ffmpeg-utils-mode-line-running-indicator (msg)
+  "Display a running indicator MSG on mode-line."
   (setq mode-line-process
         (concat " "
                 (propertize (or msg "ffmpeg running...")
@@ -93,46 +93,49 @@
   (force-mode-line-update t))
 
 ;;; ffmpeg command option "-t" accept seconds like 57 as value.
-(defun ffmpeg--subtract-timestamps (start-timestamp end-timestamp)
+(defun ffmpeg-utils--subtract-timestamps (start-timestamp end-timestamp)
   "Subtract END-TIMESTAMP with START-TIMESTAMP."
   (time-subtract
    (encode-time
-    (parse-time-string    ; (SECOND MINUTE HOUR DAY MONTH YEAR IGNORED DST ZONE)
+    (parse-time-string ; (SECOND MINUTE HOUR DAY MONTH YEAR IGNORED DST ZONE)
      (concat "2020-01-01" " " end-timestamp)))
    (encode-time
     (parse-time-string
      (concat "2020-01-01" " " start-timestamp)))))
 
-;; (ffmpeg--subtract-timestamps "00:11:25" "00:12:12")
+;; (ffmpeg-utils--subtract-timestamps "00:11:25" "00:12:12")
 
-(defun ffmpeg-cut-clip (input-filename start-timestamp end-timestamp output-filename)
-  "Cut clip of media INPUT-FILENAME between START-TIMESTAMP END-TIMESTAMP and output to OUTPUT-FILENAME.
+(defun ffmpeg-utils-cut-clip (input-filename start-timestamp end-timestamp output-filename)
+  "Clip INPUT-FILENAME on START-TIMESTAMP END-TIMESTAMP, output to OUTPUT-FILENAME.
 
 Support read timestamp begin/end range in format like this: 00:17:23 -- 00:21:45."
-  (interactive (if (region-active-p)
-                   ;; regexp spec detect "00:17:23 -- 00:21:45"
-                   (let* ((time-range (split-string
-                                       (buffer-substring-no-properties (region-beginning) (region-end))
-                                       " -- "))
-                          (timestamp-begin (car time-range))
-                          (timestamp-end (cadr time-range)))
-                     (list
-                      (expand-file-name
-                       (substring-no-properties
-                        (read-file-name "FFmpeg input filename: " nil nil 'confirm-after-completion)))
-                      timestamp-begin
-                      timestamp-end
-                      (expand-file-name
-                       (substring-no-properties
-                        (read-file-name "FFmpeg output filename: ")))))
-                 (list
-                  (read-file-name "FFmpeg input filename: ")
-                  (read-string "FFmpeg start timestamp: ")
-                  (read-string "FFmpeg end timestamp: ")
-                  (read-file-name "FFmpeg output filename: "))))
+  (interactive
+   (if (region-active-p)
+       ;; regexp spec detect "00:17:23 -- 00:21:45"
+       (let* ((time-range (split-string
+                           (buffer-substring-no-properties
+                            (region-beginning) (region-end))
+                           " -- "))
+              (timestamp-begin (car time-range))
+              (timestamp-end (cadr time-range)))
+         (list
+          (expand-file-name
+           (substring-no-properties
+            (read-file-name "FFmpeg input filename: "
+                            nil nil 'confirm-after-completion)))
+          timestamp-begin
+          timestamp-end
+          (expand-file-name
+           (substring-no-properties
+            (read-file-name "FFmpeg output filename: ")))))
+     (list
+      (read-file-name "FFmpeg input filename: ")
+      (read-string "FFmpeg start timestamp: ")
+      (read-string "FFmpeg end timestamp: ")
+      (read-file-name "FFmpeg output filename: "))))
   (when (region-active-p) (deactivate-mark) (forward-line) (newline) (forward-line -1))
-  (ffmpeg-mode-line-running-indicator "ffmpeg cut video clip")
-  (setq ffmpeg--output-filename output-filename)
+  (ffmpeg-utils-mode-line-running-indicator "ffmpeg cut video clip")
+  (setq ffmpeg-utils--output-filename output-filename)
   (let ((input-type (file-name-extension input-filename))
         (output-type (file-name-extension output-filename)))
     (if output-type
@@ -141,23 +144,24 @@ Support read timestamp begin/end range in format like this: 00:17:23 -- 00:21:45
             output-filename             ; keep output original extension
           (if (yes-or-no-p "Whether keep output video format extension? ")
               output-filename
-            (setq output-filename (format "%s.%s" (file-name-sans-extension output-filename) input-type))))
+            (setq output-filename
+                  (format "%s.%s" (file-name-sans-extension output-filename) input-type))))
       ;; output filename has NOT specific video file extension.
       (setq output-filename (format "%s.%s" output-filename input-type)))
     ;; "ffmpeg -i input-filename -ss start-timestamp -t time-timestamp -codec copy output-filename"
-    (ffmpeg--run-command
+    (ffmpeg-utils--run-command
      `("-i" ,input-filename
        "-ss" ,start-timestamp
-       "-t" ,(number-to-string (ffmpeg--subtract-timestamps start-timestamp end-timestamp))
+       "-t" ,(number-to-string (ffmpeg-utils--subtract-timestamps start-timestamp end-timestamp))
        "-codec" "copy"
        ,output-filename))))
 
 ;; (define-transient-command ffmpeg-transient ()
 ;;   "ffmpeg transient commands"
 ;;   ["Video"
-;;    ("c" "Cut video clip" ffmpeg-cut-clip)]
+;;    ("c" "Cut video clip" ffmpeg-utils-cut-clip)]
 ;;   ["Audio"
-;;    ("c" "Cut audio clip" ffmpeg-cut-clip)])
+;;    ("c" "Cut audio clip" ffmpeg-utils-cut-clip)])
 
 
 
